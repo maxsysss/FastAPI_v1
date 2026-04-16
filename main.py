f7075b010dba0bdfde889fc19c26b75515df1f99
Maximiliano <cursosdeprogramacaomax@gmail.com>
2026-04-15 23:35:11 -0300

    Authentication

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, Depends, HTTPException, status;
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import secrets
+#API Based Authentication
 from fastapi import Header;
-
 API_KEY = "mysecureapikey";
+
+#Session-Based Authentication
+from fastapi import Request, Response, Form, Cookie;
+from fastapi.responses import HTMLResponse, RedirectResponse;
+import uuid;
+
+#3.0 Simulate user and session store
+fake_user_db = {"admin": "secret"};
+session_store = {};
+
+
 
 app = FastAPI();
 
@@ -33,3 +44,32 @@ def apikey_auth(x_api_key: str = Header(...)): #Header(...) é uma função do F
             status_code=401, detail="Invalid API Key"
         )
     return {"message": "API Key authenticated!"}
+
+#3.1
+
+@app.get("/login", response_class=HTMLResponse) #response_class=HTMLResponse para ele renderizar como página web, não como JSON
+def login_form():
+    return """
+    <form action="/login" method="post">
+        Username: <input name="username">
+        Password: <input name="password" type="password">
+        <input type="submit" value="Login">
+        </form>
+        """
+
+@app.post("/login")
+def login(response: Response, username: str = Form(...), password: str = Form(...)):
+    if fake_user_db.get(username) == password:
+        session_id = str(uuid.uuid4())
+        session_store[session_id] = username
+        response = RedirectResponse(url="/dashboard", status_code=302) #encaminha para @app.get("/dashboard")
+        response.set_cookie(key="session_id", value=session_id)
+        return response;
+    raise HTTPException(status_code=401, detail="Invalid login")
+
+@app.get("/dashboard")
+def dashboard(session_id: str = Cookie(None)):
+    username = session_store.get(session_id)
+    if not username:
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    return {"message": f"Welcome {username}!"}
