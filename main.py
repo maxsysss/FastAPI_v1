ea30dd5e0dbec9754c49e034e38368cac1d10fd2
Maximiliano <cursosdeprogramacaomax@gmail.com>
2026-04-15 22:31:39 -0300

    API Key Authentication

--- main.py
+++ main.py
@@ -1,41 +1,24 @@
-# main.py — FastAPI Core Logic
+from fastapi import FastAPI, Depends, HTTPException, status;
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
 
-from fastapi import FastAPI, HTTPException
-from uuid import UUID, uuid4
-from typing import List
-from models import Book, BookCreate
-from data import fake_db
+app = FastAPI();
 
-app = FastAPI()
+security = HTTPBasic();
 
-@app.get("/books", response_model=List[Book])
-def get_books():
-    return list(fake_db.values())
+@app.get("/")
+def read_root():
+    return { "message": "Welcome to FastAPI Auth Tutorial"}
 
-@app.get("/books/{book_id}", response_model=Book)
-def get_book(book_id: UUID):
-    book = fake_db.get(book_id)
-    if not book:
-        raise HTTPException(status_code=404, detail="Book not found")
-    return book
+@app.get("/basic-auth")
+def basic_auth(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, "admin") #secrets.compare_digest() para comparação segura de strings (prevenção de "timming attacks")
+    correct_password = secrets.compare_digest(credentials.password, "secret")
 
-@app.post("/books", response_model=Book, status_code=201)
-def create_book(book: BookCreate):
-    book_id = uuid4()
-    new_book = Book(id=book_id, **book.model_dump())
-    fake_db[book_id] = new_book
-    return new_book
-
-@app.put("/books/{book_id}", response_model=Book)
-def update_book(book_id: UUID, book_data: BookCreate):
-    if book_id not in fake_db:
-        raise HTTPException(status_code=404, detail="Book not found")
-    updated_book = Book(id=book_id, **book_data.model_dump())
-    fake_db[book_id] = updated_book
-    return updated_book
-
-@app.delete("/books/{book_id}", status_code=204)
-def delete_book(book_id: UUID):
-    if book_id not in fake_db:
-        raise HTTPException(status_code=404, detail="Book not found")
-    del fake_db[book_id]
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials",
+            headers={"WWW-Authenticate":"Basic"},
+        )
+    return {"message": f"Welcome {credentials.username}"}
